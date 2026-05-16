25c2836950994bc70965333bbff906fe632a8051
Planque Léo <134111837+Pomeux@users.noreply.github.com>
2026-05-16 20:16:37 +0200

    Update 55_jumpgame.java
    change ex 55

--- leetcode/55_jumpgame.java
+++ leetcode/55_jumpgame.java
@@ -1,40 +1,23 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int[][] graph=createMatrix(nums);
-        return jump(graph,nums.length-1);
-    }
-    public boolean jump(int[][] graph,int p) {
-        System.out.println(p);
-        if(p==0)
+        if(nums.length<=1)
             return true;
-
-        for(int i=0;i<graph.length;i++) {
-            if(graph[p][i]!=0) {
-                return jump(graph,graph[p][i]-1);
+        return parcoursProfondeur(nums,0,nums.length-1);
+    }
+  
+    
+    public boolean parcoursProfondeur(int[] nums,int p,int f) {
+    
+        for(int i=1;i<=nums[p];i++) {
+            if(p+i<nums.length) {
+                if(p+i==f) {
+                    return true;
+                }
+                return parcoursProfondeur(nums,p+i,f);
+                   
+                
             }
         }
         return false;
-    }
-    public int[][] createMatrix(int[] nums) {
-        int[][] graph=new int[nums.length][nums.length];
-        int n=nums.length;
-
-
-        for(int i=0;i<n;i++) {
-            for(int j=1;j<=nums[i];j++) {
-                if(j+i<nums.length) {
-                    graph[i+j][i]=i+1;
-                }
-            }
-        }
-
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                //System.out.print(" "+graph[i][j]);
-            }
-            //System.out.println("");
-        }
-
-        return graph;
     }
 }
