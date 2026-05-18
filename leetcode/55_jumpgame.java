8b737334caac578c36b682b340d06b64181b7f0c
Planque Léo <134111837+Pomeux@users.noreply.github.com>
2026-05-18 20:54:18 +0200

    Update 55_jumpgame.java
    finish 55 but bad solution

--- leetcode/55_jumpgame.java
+++ leetcode/55_jumpgame.java
@@ -1,21 +1,31 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        boolean[] etiquette=new boolean[nums.length];
         if(nums.length<=1)
             return true;
-        return parcoursProfondeur(nums,0,nums.length-1);
+        return parcoursProfondeur(nums,0,nums.length-1,etiquette);
     }
   
     
-    public boolean parcoursProfondeur(int[] nums,int p,int f) {
-    
-        for(int i=1;i<=nums[p];i++) {
-            if(p+i<nums.length) {
-                if(p+i==f) {
-                    return true;
+    public boolean parcoursProfondeur(int[] nums,int p,int f,boolean[] etiquette) {
+        
+       
+        if(p==f) {
+          
+            return true;
+        }
+        if(!etiquette[p]) {
+            etiquette[p]=true;
+            for(int i=1;i<=nums[p];i++) {
+          
+                if(p+i<nums.length) {
+               
+                    if(parcoursProfondeur(nums,p+i,f,etiquette)) {
+                        return true;
+                    }
+                    
+                    
                 }
-                return parcoursProfondeur(nums,p+i,f);
-                   
-                
             }
         }
         return false;
